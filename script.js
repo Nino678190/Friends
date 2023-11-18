9be8102209c12e4bd06ea1b425e5e5bae230a0a9
Nino678190 <superwauzi@gmx.de>
2023-11-18 20:03:46 +0100

    Suche hinzugefügt

--- script.js
+++ script.js
@@ -5,12 +5,15 @@ function registration() {
     var email = document.getElementById("email").value
     var geburtstag = document.getElementById("geburtsdatum").value
     var pronomen = document.getElementById("pronomen").value
-    var interesse = document.getElementById("interessen").value
+    var interesse = ["minect","dwdq"]
+    //document.getElementById("interessen").value
     if (passValue === confpassValue) {
         fetch("http://192.168.22.216:8080/signup", 
         {method: "POST",
-        body: JSON.stringify({username: gametag, password: passValue, email: email, geburtstag: geburtstag, pronomen: pronomen, interessen: interesse})}).then(function(res){
-            if (res.registriert == true){
+        headers: {"content-type": "application/json"},
+        body: JSON.stringify({username: gametag, password: passValue, email: email, geburtstag: geburtstag, pronomen: pronomen, interests: interesse})}).then(function(res){
+            console.log(res)
+            if (res.status == 200){
                 location.href="/login.html"
             } else {
                 window.alert("Das hat nicht geklappt, versuche es bitte nochmal")
@@ -34,13 +37,42 @@ function verification(event){
         headers: {"content-type": "application/json"},
         body: JSON.stringify({"username": ema, "password": pas})}).then(function(res){
             console.log(res)
-            res.json().then(function(data){
-                if (data.success == true){
+                if (res.status == 200){
                     location.href="/indexa.html"
                 } 
                 else {
                     window.alert("Benutzername und/oder Passwort falsch")
                 }
             })
-        })
-    } 
+} 
+
+if (document.getElementById("freundesuche")){
+    document.getElementById("suchenform").addEventListener("submit", anfragetext)
+}
+function anfragetext(event){
+    event.preventDefault()
+    var text = document.getElementById("eingabesuche").value
+    fetch("http://192.168.22.216:8080/search", 
+        {method: "POST",
+        headers: {"content-type": "application/json"},
+        body: JSON.stringify({"search": text})})
+        .then(function(res){
+            console.log(res)
+                if (res.status == 200){
+                    res.json().then((data) => {
+                        queryUsers(data);
+                    });
+                } 
+                else {
+                    window.alert("Das hat nichts ergeben")
+                }
+            })
+
+}
+
+function queryUsers(data) {
+    for (var user of data){
+        console.log('user', user);
+    }
+}
+

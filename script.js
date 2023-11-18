5c99ddebbc74f5dbf399e68951085d87cf415f59
Nino678190 <superwauzi@gmx.de>
2023-11-18 14:23:37 +0100

    registrierung zu server theorie hinzugefügt

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-function ansValidation() {
+function registration() {
     var passValue = document.getElementById("password").value
     var confpassValue = document.getElementById("confirmPassword").value
+    var gametag = document.getElementById("gametag").value
+    var email = document.getElementById("email").value
+    var geburtstag = document.getElementById("geburtsdatum").value
+    var pronomen = document.getElementById("pronomen").value
+    var interesse = document.getElementById("interessen").value
     if (passValue === confpassValue) {
-        window.alert("Das hat geklappt")
-        return true;
+        fetch("/datenbank", 
+        {method: "POST",
+        body: JSON.stringify({username: gametag, password: pas, email: email, geburtstag: geburtstag, pronomen: pronomen, interessen: interesse})}).then(function(res){
+            if (res.registriert == true){
+                location.href="/login.html"
+            } else {
+                window.alert("Das hat nicht geklappt, versuche es bitte nochmal")
+            }
+    })
     } else {
         window.alert("Passwort stimmt nicht überein");
         return false;
     }
+    
 }
 
 function verification(){
